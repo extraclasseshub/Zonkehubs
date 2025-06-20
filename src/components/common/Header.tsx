6897df4b8c91e52e640a515a972af3b556879820
extraclasseshub <extraclasseshub@gmail.com>
2025-06-20 02:56:22 +0200

    Fix Logo Click Implementation

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -44,13 +44,16 @@ export default function Header({ onAuthClick, onLogoClick }: HeaderProps) {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center">
-              <div className="flex-shrink-0">
+              <button
+                onClick={onLogoClick}
+                className="flex-shrink-0 focus:outline-none focus:ring-2 focus:ring-[#3db2ff] rounded-md p-1 transition-all hover:scale-105"
+              >
                 <img 
                   src="/logo.png" 
                   alt="Zonke Hub" 
                   className="h-[24px] w-auto"
                 />
-              </div>
+              </button>
             </div>
 
             {/* Desktop Navigation */}
@@ -139,12 +142,6 @@ export default function Header({ onAuthClick, onLogoClick }: HeaderProps) {
                 disabled={loggingOut}
               >
                 {mobileMenuOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
-              </button>
-              <button
-                onClick={onLogoClick}
-                className="focus:outline-none focus:ring-2 focus:ring-[#3db2ff] rounded-md p-1 transition-all hover:scale-105"
-              >
-                <img />
               </button>
             </div>
           </div>
